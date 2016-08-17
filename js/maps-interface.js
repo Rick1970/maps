6c9b7619407e9258f2703473f713d9ffd185c910
rick thornbrugh and russ vetsper <rick+russ@>
2016-08-17 16:47:36 -0700

    add geolocation

--- js/maps-interface.js
+++ js/maps-interface.js
@@ -2,37 +2,65 @@
 
 var Map=require("./../js/maps.js").mapsModule;
 
+
+
 function initMap() {
-  var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
-    center: {lat: 44.540, lng: -78.546},
-    zoom: 8
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
   });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  }
 
 
-$(document).ready(function(){
-  initMap();
-  console.log("APP.JS WORKS");
 
-  $("#input-form").click(function(){
-    var city = ('#location').val();
+  $(document).ready(function(){
+    initMap();
+    console.log("APP.JS WORKS");
+
+    $("#input-form").click(function(){
+      var city = ('#location').val();
+
+    });
+
+    $("#time").text(moment());
+
+    // $(document).ready(function(){
+    // });
+
+
+
+
+    function update(){
+      $("#clock").html(moment().format("D. MMMM YYYY H:mm:ss"));
+    }
+    setInterval(update, 1000);
+
 
   });
-  
-  $("#time").text(moment());
-
-  // $(document).ready(function(){
-  // });
-
-
-
-
-  function update(){
-    $("#clock").html(moment().format("D. MMMM YYYY H:mm:ss"));
-  }
-  setInterval(update, 1000);
-
-
-});
